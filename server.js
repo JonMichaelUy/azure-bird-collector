82484a684ea637888f923b6bbe2281903e68ab4d
Jon Michael Uy <uy.jonmichael@gmail.com>
2022-05-30 13:51:49 -0400

    create main index page

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+//Dependencies
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const birdsController = require('./controllers/birds');
+
+
+//Database Connection
+
+//Controllers
+app.use('/', birdsController)
 
 
 
@@ -12,7 +20,9 @@ const PORT = 3000;
 
 
 
-
+app.get('/', (req, res) => {
+  res.render('index.ejs')
+});
 
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`)
