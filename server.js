c25002a004d2b84e72677d2add797ad16f7a0aa1
Jon Michael Uy <uy.jonmichael@gmail.com>
2022-06-01 21:29:18 -0400

    add partials for css

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 //Controllers
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
 app.use('/', birdsController)
 
 
