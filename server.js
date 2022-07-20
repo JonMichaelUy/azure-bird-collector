42a90f94bff846a2cd46f97e4c3600b316d3bb9f
Jon Michael Uy <uy.jonmichael@gmail.com>
2022-07-19 21:13:31 -0400

    deploy in msft azure, deploy key vault

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 //Dependencies
-require('dotenv').config();
+const dotenv = require("dotenv");
+dotenv.config();
+// require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
 const birdsController = require('./controllers/birds.js');
@@ -7,11 +9,31 @@ const app = express();
 const methodOverride = require("method-override");
 const PORT = process.env.PORT || 3000
 
-//Database Connection
-mongoose.connect(process.env.MONGODB_URI, {
-	useUnifiedTopology: true,
-});
+const { SecretClient } = require("@azure/keyvault-secrets");
+const { DefaultAzureCredential } = require("@azure/identity");
+// const { objectValues } = require("@azure/ms-rest-js/es/lib/util/utils.js");
 
+async function main() {
+
+  const credential = new DefaultAzureCredential();
+  const url = "https://mikekeyvault-1.vault.azure.net";
+  const client = new SecretClient(url, credential);
+  const secret = await client.getSecret("secretdburi");
+  console.log("secret: ", secret);
+
+  mongoose.connect(secret.value, {
+    useUnifiedTopology: true,
+  });
+  
+
+}
+
+main().catch((error) => {
+  console.error("An error occurred:", error);
+  process.exit(1);
+});
+// mongoose.connect(secret, {
+//Database Connection
 const db = mongoose.connection;
 db.on('error', (err) => console.log(err.message + ' is mongodb not running?'));
 db.on('connected', () => console.log('mongo connected'));
@@ -21,6 +43,7 @@ app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
 app.use('/', birdsController)
+
 
 
 
